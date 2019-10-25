2ff4e1730bb2a22943200565661b5438d4ae0ab6
Eric-Guo <eric.guocz@gmail.com>
2019-10-25 14:33:25 +0800

    Review remain position functions

--- lib/yxt-api/position.rb
+++ lib/yxt-api/position.rb
@@ -3,11 +3,7 @@ module Yxt
     request 'v1/udp/sy/position', datas: position_hash
   end
 
-  def self.sync_position_for_no_pno(position_hash)
-    request 'el/sync/positionfornopno', positionInfo: position_hash.to_json.to_s
-  end
-
-  def self.update_position_name(position_no, position_name)
-    request 'el/sync/updatepositioninfo', positionNo: position_no, positionName: position_name
+  def self.update_position_info(position_no, position_name)
+    request 'v1/udp/sy/updatepositioninfo', positionNo: position_no, positionName: position_name
   end
 end
