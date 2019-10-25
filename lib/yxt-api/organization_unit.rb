b83f854c0c9f6236ef992a57705bb5c8319ffcea
Eric-Guo <eric.guocz@gmail.com>
2019-10-25 14:22:05 +0800

    Review API which is not used by oauth2id.

--- lib/yxt-api/organization_unit.rb
+++ lib/yxt-api/organization_unit.rb
@@ -4,18 +4,19 @@ module Yxt
   end
 
   def self.delete_ous(ou_code_or_third_system_ids)
-    request 'el/sync/deleteous', OuCodeOrThirdSystemID: ou_code_or_third_system_ids.to_json.to_s
+    request 'v1/udp/sy/deleteous', datas: ou_code_or_third_system_ids
   end
 
-  def self.remove_user_from_all_ous(user_names)
-    request 'el/sync/removeusersfromou', userNames: user_names.to_json.to_s
+  def self.remove_users_from_ou(user_names)
+    request 'v1/udp/sy/removeusersfromou', datas: user_names
   end
 
-  def self.batch_change_org_ou(new_ou_id, user_names)
-    request 'el/sync/batchchangeorgou', newOuID: new_ou_id, userNames: user_names.to_json.to_s
+  def self.batch_change_org_ou(user_names, extend_key, tool_user_id)
+    request 'v1/udp/sy/batchchangeorgou',
+            userNames: user_names, extendKey: extend_key, toolUserId: tool_user_id
   end
 
-  def self.get_ou_code_by_ou_name(ou_name, islink: true)
-    request 'el/sync/getoucodebyouname', islink: islink, ouname: ou_name
+  def self.get_ou_code_by_ou_name(extend_key, page_index = 1, page_size = 30)
+    request 'v1/udp/sy/deptout', extendKey: extend_key, pageIndex: page_index, pageSize: page_size
   end
 end
