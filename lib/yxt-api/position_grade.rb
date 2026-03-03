add83ab7bab7d45d045db2ab74ea932d7fc90200
Eric-Guo <eric.guocz@gmail.com>
2026-03-03 16:26:58 +0800

    Add positiongrades_del

--- lib/yxt-api/position_grade.rb
+++ lib/yxt-api/position_grade.rb
@@ -5,4 +5,8 @@ module Yxt
   def self.positiongrades_sync(positiongrades_hash)
     request 'v1/udp/public/positiongrades/sync', positiongrades_hash
   end
+
+  def self.positiongrades_del(thirdId)
+    request "v1/udp/public/positiongrades/#{thirdId}/del", {}
+  end
 end
