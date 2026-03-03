ebafb63c9b8d275e2dba973a01d4af85ee9b0383
Eric-Guo <eric.guocz@gmail.com>
2026-03-03 16:20:52 +0800

    No slash at begin.

--- lib/yxt-api/position_grade.rb
+++ lib/yxt-api/position_grade.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module Yxt
+  # https://open.yunxuetang.cn/#/document?&id=1643935705069297666
   def self.positiongrades_sync(positiongrades_hash)
-    request '/v1/udp/public/positiongrades/sync', positiongrades_hash
+    request 'v1/udp/public/positiongrades/sync', positiongrades_hash
   end
 end
