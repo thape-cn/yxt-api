c5c9b04a09fb91ee5c97e5a956db044c3e2b856d
Eric-Guo <eric.guocz@gmail.com>
2026-03-04 18:16:50 +0800

    Fix

--- lib/yxt-api/position.rb
+++ lib/yxt-api/position.rb
@@ -3,6 +3,6 @@
 module Yxt
   # https://open.yunxuetang.cn/#/document?id=1643934601388531714
   def self.positions_sync(position_hash)
-    request 'v1/udp/public/positions/sync', datas: position_hash
+    request 'v1/udp/public/positions/sync', position_hash
   end
 end
