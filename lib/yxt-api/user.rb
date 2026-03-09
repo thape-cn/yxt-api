73b6b65e009dc4abd7a902e4bd837b3068016ddc
Eric-Guo <eric.guocz@gmail.com>
2026-03-09 15:21:51 +0800

    New users_recoversync

--- lib/yxt-api/user.rb
+++ lib/yxt-api/user.rb
@@ -1,22 +1,7 @@
 # frozen_string_literal: true
 
 module Yxt
-  def self.create_user(users_hash, islink: 1, is_send_notice: 0)
-    request 'v1/udp/sy/sv/use', islink: islink, isSendNotice: is_send_notice,
-                                datas: users_hash.collect { |h| h.merge(isEmailValidated: 1, isMobileValidated: 1) }
-  end
-
-  def self.sync_users(users_hash, islink: 1, is_send_notice: 0)
-    request 'v1/udp/sy/users', islink: islink, isSendNotice: is_send_notice,
-                               datas: users_hash
-  end
-
-  def self.disable_users(user_names, is_clear_email_and_mobile: 0)
-    request 'v1/udp/sy/disabledusers', isClearEmailAndMobile: is_clear_email_and_mobile,
-                                       datas: user_names
-  end
-
-  def self.enable_users(user_names)
-    request 'v1/udp/sy/enabledusers', datas: user_names
+  def self.users_recoversync(user_hash)
+    request 'v1/udp/public/users/recoversync', user_hash
   end
 end
